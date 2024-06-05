436b39d426c075eb3438234ecf6e802b4385908b
Lucas Santos <161741045+LuzKS@users.noreply.github.com>
2024-06-05 19:01:31 -0300

    e isso ai

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,13 +1,41 @@
-const formGeral = document.querySelector("form");
-formGeral.onsubmit = (ev) => {
+const butao = document.querySelector("button");
+butao.addEventListener("click", function(){salvarDados()}) 
+
+console.log("oiiii")
+function salvarDados(){
+    console.log("entramo")
     uNome = document.querySelector("#uNome").value;
     uEmail = document.querySelector("#uEmail").value;
     uSenha = document.querySelector("#uSenha").value;
     uConfSenha = document.querySelector("#uConfSenha").value;
+    console.log(uNome)
 
-    const dados = {nome: uNome, email: uEmail, senha: uSenha, confsenha: uConfSenha};
-    console.log(dados);
+    if (uSenha != uConfSenha){
+        alert("senhas não coincidem")
+    }
+
+    const dadosArmazenados = localStorage.getItem('usuarioDados');
+    if (dadosArmazenados) {
+        const usuarios = JSON.parse(dadosArmazenados);
+        if (usuarios.nome === uNome) {
+            alert("Nome de usuário já em uso.");
+            return;
+        }
+        if (usuarios.email === uEmail) {
+            alert("Endereço de e-mail já em uso.");
+            return;
+        }
+    }
+
+    const dados = {
+        nome: uNome, 
+        email: uEmail, 
+        senha: uSenha, 
+        confsenha: uConfSenha
+    };
+        
     const dadosString = JSON.stringify(dados);
     localStorage.setItem('usuarioDados', dadosString);
 
 };
+
