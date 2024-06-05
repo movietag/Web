c3a75cb99b5879f5908f437ff46025d4485c6122
movietag <movietag.pi@gmail.com>
2024-06-05 17:46:36 -0300

    .

--- js/buscaAvancada.js
+++ js/buscaAvancada.js
@@ -1,10 +1,9 @@
 //mostrar e ocultar os inputs dos filtros
 const filtros = document.querySelectorAll(".divInput");
 const setas = document.querySelectorAll(".rotulo");
-const setinhaBaixo = document.querySelectorAll("bx-chevron-down");
 
 //repeticao pegando todas as setas e adicionando o evento de click para cada rotulo
-for (let i = 0; i<setas.length; i++){
+for (i = 0; i<setas.length; i++){
     setas[i].addEventListener("click", function(){mostraFiltro(setas[i], filtros[i])});
 }
 
@@ -17,6 +16,7 @@ function mostraFiltro(div, elem){
     trocaSeta(div);
 }
 
+//gira as setas de acordo com se o input está mostrando ou nao
 function trocaSeta(div){
     div.lastChild.classList.toggle("cima");
 }
@@ -30,3 +30,31 @@ botao.addEventListener("click", function(){
     antes.style.display = "none";
     resultados.style.display = "block";
 });
+
+//mostrando todos os filtros
+const mostrar = document.querySelector("#botaoMostrar");
+mostrar.addEventListener("click", function(){mostrarTodos(mostrar, esconder, filtros, setas)});
+function mostrarTodos(mos, esc, filt, set){
+    for (i = 0; i < filt.length; i++){
+        filt[i].style.display = "block";
+    }
+    for (i = 0; i < set.length; i++){
+        set[i].lastChild.classList.add("cima")
+    }
+    mos.style.display = "none";
+    esc.style.display = "block";
+}
+
+//ocultando todos os filtros
+const esconder = document.querySelector("#botaoEsconder");
+esconder.addEventListener("click", function(){esconderTodos(mostrar, esconder, filtros, setas)});
+function esconderTodos(mos, esc, filt, set){
+    for (i = 0; i < filt.length; i++){
+        filt[i].style.display = "none";
+    }
+    for (i = 0; i < set.length; i++){
+        set[i].lastChild.classList.remove("cima");
+    }
+    mos.style.display = "block";
+    esc.style.display = "none";
+}
