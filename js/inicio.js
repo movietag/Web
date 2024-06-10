6b214805ce91847a5ca1e1f5dad784fed89c7ea4
Guilherme Costa <guiihocosta@gmail.com>
2024-06-10 17:13:19 -0300

    API

--- js/inicio.js
+++ js/inicio.js
@@ -1,31 +1,29 @@
-const fPesquisa = document.querySelector("form");
-const bPesquisar = document.querySelector("#pesq1");
-const lista = document.querySelector(".lista");
+// Pegando os Elementos na DOM
+const fPesquisa = document.querySelector("form"); // Formulário da Pesquisa
+const lista = document.querySelector(".lista"); // Pega  a div em que o conteúdo 'Popular' aparece (Apenas para demonstração)
 
-bPesquisar.addEventListener("click", pesquisa);
+// Adicionando Eventos
+fPesquisa.onsubmit = (ev) =>{ // Quando a pesquisa é submetida...
+    ev.preventDefault(); // Não permite que a página reenicie
+    const resultados = document.querySelector("#s02 .lista"); // Pega a div dos resultados para a preencher
+    let busca = ev.target.pesquisa.value; // Pega o valor de dentro da pesquisa
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${busca}&include_adult=false&language=pt-BR&page=1`, options) // API, enviando a busca
+        .then(response => response.json())
+        .then(json => carregaFilmes(resultados, json))
 
+    console.log(resultados);
+    pesquisa();
+}
+
+
+// Funções 
 function pesquisa(){
     const areas = document.querySelectorAll(".area");
     areas[0].style.display = "none";
     areas[1].style.display = "block";
 }
 
-// API
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZWExZjFhNGRjYzY2M2EzMTRlNmUwMDhhOGI5YWEyYyIsInN1YiI6IjY1ZDkxNjJiMGYwZGE1MDE2MjMyMjViMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qCV25ZQQfVb7YOVnVVFI_xn7MAnNYMbZRrj0SApcL7k'
-    }
-  };
-
-fetch('https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1', options)
-    .then(response => response.json())
-    .then(json => carregaLista(json))
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
-const carregaLista = (json) => {
+const carregaFilmes = (lista, json) => { //Carrega o JSON, guardando os elementos na div correspondente
     const itens = lista.lastElementChild;
     itens.replaceChildren();
 
@@ -41,3 +39,18 @@ const carregaLista = (json) => {
         itens.appendChild(item);
     });
 }
+
+// API
+const options = { //Opções enviadas para a API
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZWExZjFhNGRjYzY2M2EzMTRlNmUwMDhhOGI5YWEyYyIsInN1YiI6IjY1ZDkxNjJiMGYwZGE1MDE2MjMyMjViMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qCV25ZQQfVb7YOVnVVFI_xn7MAnNYMbZRrj0SApcL7k'
+    }
+  };
+
+fetch('https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1', options)
+    .then(response => response.json())
+    .then(json => carregaFilmes(lista, json))
+
+
--- js/visualizacaoProducao.js
+++ js/visualizacaoProducao.js
@@ -13,15 +13,47 @@ icon.addEventListener("click", (ev) => {
     }
 });
 
-API
+// Testes
+// // API
 
-function queryObj() { // Pega os valores do link HTML
-    var result = {}, keyValuePairs = location.search.slice(1).split("&");
-    keyValuePairs.forEach(function(keyValuePair) {
-        keyValuePair = keyValuePair.split('=');
-        result[decodeURIComponent(keyValuePair[0])] = decodeURIComponent(keyValuePair[1]) || '';
-    });
-    return result;
-}
-var myParam = queryObj();
-console.log(myParam);
+// var myParam = queryObj();
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZWExZjFhNGRjYzY2M2EzMTRlNmUwMDhhOGI5YWEyYyIsInN1YiI6IjY1ZDkxNjJiMGYwZGE1MDE2MjMyMjViMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qCV25ZQQfVb7YOVnVVFI_xn7MAnNYMbZRrj0SApcL7k'
+//     }
+//   };
+  
+// fetch(`https://api.themoviedb.org/3/movie/${myParam.query}?append_to_response=20&language=pt-BR`, options)
+//     .then(response => response.json())
+//     .then(json => carregaDados(json))
+
+// function queryObj() { // Pega os valores do link HTML
+//     var result = {}, keyValuePairs = location.search.slice(1).split("&");
+//     keyValuePairs.forEach(function(keyValuePair) {
+//         keyValuePair = keyValuePair.split('=');
+//         result[decodeURIComponent(keyValuePair[0])] = decodeURIComponent(keyValuePair[1]) || '';
+//     });
+//     return result;
+// }
+
+// // Atualizando Dados a partir da API
+// function carregaDados(json){
+//     const pai = document.querySelector(".main-banner_banner_contents");
+//     pai.firstElementChild.firstElementChild.setAttribute("src", `https://image.tmdb.org/t/p/w300${json.poster_path}`); // Atualiza Banner Principal
+    
+//     pai.parentElement.parentElement.style.backgroundColor = "#efefef";
+//     console.log(`https://image.tmdb.org/t/p/w300${json.backdrop_path}`);
+//     console.log(pai.parentElement.parentElement);
+
+//     const dados = pai.lastElementChild.firstElementChild;
+//     const title = dados.firstElementChild.firstElementChild;
+
+//     title.firstElementChild.innerHTML = `${json.title}`; // Título
+//     const date = new Date(json.release_date); // Ano de Estréia
+//     title.lastElementChild.innerHTML = `${date.getFullYear()}`
+
+//     console.log(title.nextElementSibling)
+// }
