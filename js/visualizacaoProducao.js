770d8c32502af9bb58e1cec55202b044cc2660a5
Guilherme Costa <guiihocosta@gmail.com>
2024-06-11 08:54:45 -0300

    Js

--- js/visualizacaoProducao.js
+++ js/visualizacaoProducao.js
@@ -16,44 +16,38 @@ icon.addEventListener("click", (ev) => {
 // Testes
 // // API
 
-// var myParam = queryObj();
+var myParam = queryObj();
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//       accept: 'application/json',
-//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZWExZjFhNGRjYzY2M2EzMTRlNmUwMDhhOGI5YWEyYyIsInN1YiI6IjY1ZDkxNjJiMGYwZGE1MDE2MjMyMjViMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qCV25ZQQfVb7YOVnVVFI_xn7MAnNYMbZRrj0SApcL7k'
-//     }
-//   };
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZWExZjFhNGRjYzY2M2EzMTRlNmUwMDhhOGI5YWEyYyIsInN1YiI6IjY1ZDkxNjJiMGYwZGE1MDE2MjMyMjViMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qCV25ZQQfVb7YOVnVVFI_xn7MAnNYMbZRrj0SApcL7k'
+    }
+  };
   
-// fetch(`https://api.themoviedb.org/3/movie/${myParam.query}?append_to_response=20&language=pt-BR`, options)
-//     .then(response => response.json())
-//     .then(json => carregaDados(json))
+fetch(`https://api.themoviedb.org/3/movie/${myParam.query}?append_to_response=20&language=pt-BR`, options)
+    .then(response => response.json())
+    .then(json => carregaDados(json))
 
-// function queryObj() { // Pega os valores do link HTML
-//     var result = {}, keyValuePairs = location.search.slice(1).split("&");
-//     keyValuePairs.forEach(function(keyValuePair) {
-//         keyValuePair = keyValuePair.split('=');
-//         result[decodeURIComponent(keyValuePair[0])] = decodeURIComponent(keyValuePair[1]) || '';
-//     });
-//     return result;
-// }
+function queryObj() { // Pega os valores do link HTML
+    var result = {}, keyValuePairs = location.search.slice(1).split("&");
+    keyValuePairs.forEach(function(keyValuePair) {
+        keyValuePair = keyValuePair.split('=');
+        result[decodeURIComponent(keyValuePair[0])] = decodeURIComponent(keyValuePair[1]) || '';
+    });
+    return result;
+};
 
-// // Atualizando Dados a partir da API
-// function carregaDados(json){
-//     const pai = document.querySelector(".main-banner_banner_contents");
-//     pai.firstElementChild.firstElementChild.setAttribute("src", `https://image.tmdb.org/t/p/w300${json.poster_path}`); // Atualiza Banner Principal
-    
-//     pai.parentElement.parentElement.style.backgroundColor = "#efefef";
-//     console.log(`https://image.tmdb.org/t/p/w300${json.backdrop_path}`);
-//     console.log(pai.parentElement.parentElement);
+// Atualizando Dados a partir da API
+function carregaDados(json){
+    console.log(`https://image.tmdb.org/t/p/w300${json.backdrop_path}`);
 
-//     const dados = pai.lastElementChild.firstElementChild;
-//     const title = dados.firstElementChild.firstElementChild;
+    // Pegando os elementos na Tela
+    const titulo = document.querySelector(".titulo");
+    const anoLancamento = document.querySelector(".ano");
 
-//     title.firstElementChild.innerHTML = `${json.title}`; // Título
-//     const date = new Date(json.release_date); // Ano de Estréia
-//     title.lastElementChild.innerHTML = `${date.getFullYear()}`
-
-//     console.log(title.nextElementSibling)
-// }
+    titulo.innerHTML = `${json.title}`; // Título
+    const date = new Date(json.release_date); // Ano de Estréia
+    anoLancamento. innerHTML = `(${date.getFullYear()})`
+};
